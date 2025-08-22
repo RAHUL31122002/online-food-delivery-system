732455cff23aaf89d07c7080b11c3deeee31608d
RAHUL KUMAR <rahul31122002@gmail.com>
2025-08-22 22:26:34 +0530

    Create JwtAuthFilter.java

---
+++ security/security/JwtAuthFilter.java
@@ -0,0 +1,10 @@
+package com.foodapp.security;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springfr
